a53c9ba802b62312f96c76c8f80d0f644d77198b
comp500 <comp500@users.noreply.github.com>
2019-09-14 02:22:51 +0100

    Add proper pick block / block drop support with tooltip

--- src/main/java/link/infra/demagnetize/blocks/Demagnetizer.java
+++ src/main/java/link/infra/demagnetize/blocks/Demagnetizer.java
@@ -1,20 +1,32 @@
 package link.infra.demagnetize.blocks;
 
+import link.infra.demagnetize.Demagnetize;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.network.NetworkHooks;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class Demagnetizer extends Block {
 	private final boolean isAdvanced;
@@ -64,69 +76,39 @@ public class Demagnetizer extends Block {
 			((DemagnetizerTileEntity) te).updateRedstone(powerLevel > 0);
 		}
 	}
-//
-//	// https://twitter.com/McJty/status/1002546886161596416
-//	@Override
-//	public void getDrops(@Nonnull NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, @Nonnull BlockState state, int fortune) {
-//		TileEntity te = world.getTileEntity(pos);
-//		if (te instanceof DemagnetizerTileEntity) {
-//			ItemStack stack = new ItemStack(Item.getItemFromBlock(this));
-//			CompoundNBT tagCompound = new CompoundNBT();
-//			// writeInternalNBT must be used to avoid setting the block position
-//			((DemagnetizerTileEntity) te).writeInternalNBT(tagCompound);
-//
-//			stack.setTagCompound(tagCompound);
-//			drops.add(stack);
-//		} else {
-//			super.getDrops(drops, world, pos, state, fortune);
-//		}
-//	}
-//
-//	@Override
-//	public boolean removedByPlayer(@Nonnull BlockState state, World world, @Nonnull BlockPos pos, @Nonnull PlayerEntity player, boolean willHarvest) {
-//		if (willHarvest) {
-//			return true;
-//		}
-//		return super.removedByPlayer(state, world, pos, player, false);
-//	}
-//
-//	@Override
-//	public void harvestBlock(@Nonnull World worldIn, PlayerEntity player, @Nonnull BlockPos pos, @Nonnull BlockState state, @Nullable TileEntity te, ItemStack stack) {
-//		super.harvestBlock(worldIn, player, pos, state, te, stack);
-//		worldIn.setBlockToAir(pos);
-//	}
-//
-//	@Override
-//	public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
-//		TileEntity te = worldIn.getTileEntity(pos);
-//		if (te instanceof DemagnetizerTileEntity && stack.getTagCompound() != null) {
-//			// readInternalNBT must be used to avoid setting the block position
-//			((DemagnetizerTileEntity) te).readInternalNBT(stack.getTagCompound());
-//		}
-//	}
-//
-//	@Override
-//	@SideOnly(Side.CLIENT)
-//	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-//		if (stack.getTagCompound() != null) {
-//			tooltip.add(TextFormatting.ITALIC + I18n.format("tooltip." + Demagnetize.MODID + ".configured") + TextFormatting.RESET);
-//		}
-//	}
-//
-//	@Nonnull
-//	@Override
-//	public ItemStack getPickBlock(@Nonnull BlockState state, RayTraceResult target, World world, @Nonnull BlockPos pos, PlayerEntity player) {
-//		TileEntity te = world.getTileEntity(pos);
-//		if (te instanceof DemagnetizerTileEntity) {
-//			ItemStack stack = new ItemStack(Item.getItemFromBlock(this));
-//			CompoundNBT tagCompound = new CompoundNBT();
-//			// writeInternalNBT must be used to avoid setting the block position
-//			((DemagnetizerTileEntity) te).writeInternalNBT(tagCompound);
-//
-//			stack.setTagCompound(tagCompound);
-//			return stack;
-//		}
-//		return super.getPickBlock(state, target, world, pos, player);
-//	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable IBlockReader worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		if (stack.getTag() != null) {
+			tooltip.add(new TranslationTextComponent("tooltip." + Demagnetize.MODID + ".configured").applyTextStyles(TextFormatting.ITALIC, TextFormatting.GRAY));
+		}
+	}
+
+	@Nonnull
+	@Override
+	public ItemStack getPickBlock(BlockState state, RayTraceResult target, IBlockReader world, BlockPos pos, PlayerEntity player) {
+		TileEntity te = world.getTileEntity(pos);
+		if (te instanceof DemagnetizerTileEntity) {
+			ItemStack stack = new ItemStack(this);
+			CompoundNBT tagCompound = new CompoundNBT();
+			CompoundNBT tagCompoundFull = new CompoundNBT();
+			te.write(tagCompoundFull);
+
+			// Copy only specific tags to tagCompound
+			if (tagCompoundFull.contains("items"))
+				tagCompound.put("items", tagCompoundFull.getCompound("items"));
+			if (tagCompoundFull.contains("redstone"))
+				tagCompound.putString("redstone", tagCompoundFull.getString("redstone"));
+			if (tagCompoundFull.contains("range"))
+				tagCompound.putInt("range", tagCompoundFull.getInt("range"));
+			if (tagCompoundFull.contains("whitelist"))
+				tagCompound.putBoolean("whitelist", tagCompoundFull.getBoolean("whitelist"));
+
+			stack.setTagInfo("BlockEntityTag", tagCompound);
+			return stack;
+		}
+		return super.getPickBlock(state, target, world, pos, player);
+	}
 
 }
